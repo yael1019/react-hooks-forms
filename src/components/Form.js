0969156d88de4664f326a7a0c5c66ca9515b2306
Yael Harosh <yaelharosh01@gmail.com>
2023-03-16 13:17:06 -0400

    finish

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,14 +1,38 @@
 import React, { useState } from "react";
 
+// function Form({
+//   firstName,
+//   lastName,
+//   handleFirstNameChange,
+//   handleLastNameChange,
+// }) {
+//   return (
+//     <form>
+//       <input type="text" onChange={handleFirstNameChange} value={firstName} />
+//       <input type="text" onChange={handleLastNameChange} value={lastName} />
+//       <button type="submit">Submit</button>
+//     </form>
+//   );
+// }
+
 function Form() {
-  const [firstName, setFirstName] = useState("John");
-  const [lastName, setLastName] = useState("Henry");
+  const [number, setNumber] = useState(0);
+  const [isInvalidNumber, setIsInvalidNumber] = useState(null);
+
+  function handleNumberChange(event) {
+    const newNumber = parseInt(event.target.value);
+    if (newNumber >= 0 && newNumber <= 5) {
+      setNumber(newNumber);
+      setIsInvalidNumber(null);
+    } else {
+      setIsInvalidNumber(`${newNumber} is not a valid number!`);
+    }
+  }
 
   return (
     <form>
-      <input type="text" value={firstName} />
-      <input type="text" value={lastName} />
-      <button type="submit">Submit</button>
+      <input type="number" value={number} onChange={handleNumberChange} />
+      {isInvalidNumber ? <span style={{ color: "red" }}>{isInvalidNumber}</span> : null}
     </form>
   );
 }
